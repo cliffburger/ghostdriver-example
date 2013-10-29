32348384ef9cb1d8c80d7abcff4e8c8d17ed41b2
Cliff Burger <cliffburger>
2013-10-28 17:35:35 -0700

    First test of ghostdriver in C#

---
+++ dot-net/ghostdriver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+using log4net;
+using log4net.Config;
+
+namespace GhostDriver.Example
+{
+    class Program
+    {
+        private static ILog Log = LogManager.GetLogger(typeof (Program));
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            PhantomJSDriver driver = new PhantomJSDriver();
+            driver.Navigate().GoToUrl("https://duckduckgo.com/");
+            Log.InfoFormat("Your title is ... {0}", driver.Title);
+
+            var searchElement = driver.FindElementById("search_form_input_homepage");
+            searchElement.SendKeys("Why duck go?");
+            searchElement.Submit();
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(d => ((PhantomJSDriver)d).FindElementById("search_form_input"));
+
+            Log.InfoFormat("Your new title is ... {0}", driver.Title);
+
+            driver.Dispose();
+        }
+    }
+}
---
+++ dot-net/ghostdriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ghostdriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("ghostdriver")]
+[assembly: AssemblyCopyright("Copyright © Cliff Burger 2013")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
